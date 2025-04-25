918e674676938cb09669f2c2224fcecb0c471a2e
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2025-04-25 00:39:54 -0400

    update registro conductor

--- web-admin/src/app/services/auth.service.ts
+++ web-admin/src/app/services/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut, signInWithEmailAndPassword } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from '../../../../firebase/firebase-config'; // Ruta corregida
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { firebaseConfig } from '../../../../firebase/firebase-config';
+import { Conductor } from '../models/models';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 @Injectable({
   providedIn: 'root'
@@ -24,14 +27,32 @@ export class AuthService {
     }
   }
 
-  async register(email: string, password: string): Promise<void> {
+  async registerConductor(conductor: Conductor): Promise<void> {
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      console.log('Registro exitoso');
-      console.log('Correo:', userCredential.user.email);
-      console.log('ID:', userCredential.user.uid);
+      // Crear usuario en Firebase Authentication con una contraseña temporal
+      const userCredential = await createUserWithEmailAndPassword(auth, conductor.correo, 'Temporal123!');
+      const userId = userCredential.user.uid;
+  
+      // Guardar los datos en la colección "usuarios"
+      await setDoc(doc(db, 'USUARIO', userId), {
+        id: userId,
+        correo: conductor.correo,
+        perfil: 'Conductor' // Siempre será "Conductor" en este caso
+      });
+  
+      // Guardar los datos en la colección "CONDUCTOR"
+      await setDoc(doc(db, 'CONDUCTOR', userId), {
+        nombre: conductor.nombre,
+        rut: conductor.rut,
+        telefono: conductor.telefono,
+        licencia: conductor.licencia
+      });
+  
+      // Enviar correo de restablecimiento de contraseña
+      await sendPasswordResetEmail(auth, conductor.correo);
+      console.log('Conductor registrado en ambas colecciones y correo de restablecimiento enviado.');
     } catch (error) {
-      console.error('Error en el registro:', error);
+      console.error('Error al registrar al conductor:', error);
       throw error;
     }
   }
